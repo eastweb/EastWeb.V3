757e327db28458659bf16a7c6016acbc51987004
fangyu <bravefishing@gmail.com>
2015-06-15 11:59:04 -0500

    implement ModisLSTFIlter

--- src/version2/prototype/projection/ModisLST/ModisLSTFilter.java
+++ src/version2/prototype/projection/ModisLST/ModisLSTFilter.java
@@ -12,21 +12,66 @@ public class ModisLSTFilter extends Filter{
     @Override
     protected double filterValue(double value) {
         //in data bands the data type is 16bits unsigned integer
-        if (value < 7500 || value > 65535) {
-            return 65536;
-        } else {
-            return value;
-        }
+        return 0;
     }
 
     @Override
     protected double filterByQCFlag(String qcLevel) {
         // TODO Auto-generated method stub
-
-        return 0;
+        //the value of qcBands is a 8-didit value
+        // int[] value=qcBands;
+        // QCLevel qc=ConvertQCLevel(getQCLevel());
+        String qclevel=getQCLevel();
+        if(qcLevel.equals(qclevel)==true) {
+            return 0;
+        } else {
+            return -9999;
+        }
     }
 
 
+    public QCLevel ConvertQCLevel(String qclevel)
+    {
+        if(qclevel=="HIGHEST") {
+            return QCLevel.HIGHEST;
+        } else if(qclevel=="MODERATE") {
+            return QCLevel.MODERATE;
+        } else if(qclevel=="LOW") {
+            return QCLevel.LOW;
+        } else if(qclevel=="NONE"){
+            return QCLevel.NONE;
+        } else {
+            return null;
+        }
+    }
 
-
+    public String getQCLevel()
+    {
+        int []i=new int[8];
+        i=qcBands;
+        boolean h=false;
+        boolean m=false;
+        boolean l=false;
+        String qclevel=null;
+        if(i[6]==0 && i[7]==0)
+        {
+            h=true;
+            qclevel="HIGHEST";
+        }
+        if((i[6]==0 && i[7]==1 && i[0]==0)||h==true)
+        {
+            m=true;
+            qclevel="MODERATE";
+        }
+        if((i[6]==0 && i[7]==1 && i[0]==1 && i[1]==0)||m==true)
+        {
+            l=true;
+            qclevel="LOW";
+        }
+        if((i[6]==0 && i[7]==1 && i[0]==1 && i[1]==1)||l==true)
+        {
+            qclevel="NONE";// no screening
+        }
+        return qclevel;
+    }
 }
