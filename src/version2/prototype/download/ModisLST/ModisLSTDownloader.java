f43351aabe561c3de3637a6791ed4ca58931c2c8
fangyu <bravefishing@gmail.com>
2015-07-06 09:15:05 -0500

    update ModisLSTDownloader and test file

--- src/version2/prototype/download/ModisLST/ModisLSTDownloader.java
+++ src/version2/prototype/download/ModisLST/ModisLSTDownloader.java
@@ -1,53 +1,44 @@
 package version2.prototype.download.ModisLST;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.ConnectException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.io.FileUtils;
 import org.xml.sax.SAXException;
 
-import nu.validator.htmlparser.dom.HtmlDocumentBuilder;
 import version2.prototype.ConfigReadException;
 import version2.prototype.DataDate;
 import version2.prototype.ModisTile;
-import version2.prototype.Config;
-import version2.prototype.PluginMetaData.PluginMetaDataCollection.DownloadMetaData;
-import version2.prototype.download.ConnectionContext;
 import version2.prototype.download.DownloadFailedException;
 import version2.prototype.download.DownloadUtils;
 import version2.prototype.download.DownloaderFramework;
+import version2.prototype.util.ParallelUtils.Parallel;
 
-
-public class ModisLSTDownloader extends DownloaderFramework{
-
-    private static final String PRODUCT = "MOD11A2";
-    private static final String getModisDateDir(String rootDir, DataDate date) {
-        return String.format("%s/%04d.%02d.%02d", rootDir, date.getYear(),
-                date.getMonth(), date.getDay());
-    }
-
+public final class ModisLSTDownloader extends DownloaderFramework {
     private final DataDate mDate;
     private final ModisTile mTile;
-    private final File mOutFile;
-    private static DownloadMetaData metaData;
+    private final String mOutFolder;
+    private final String mOutQCFolder;
+    private static String mHostURL;
+    private String mMode;
 
-    public ModisLSTDownloader(DataDate date, ModisTile tile,
-            File outFile,DownloadMetaData data) {
+    public ModisLSTDownloader(DataDate date, ModisTile tile, String outFolder,String qcOutFolder, String mode, String hostURL) {
         mDate = date;
         mTile = tile;
-        mOutFile = outFile;
-        metaData = data;
+        mOutFolder = outFolder;
+        mOutQCFolder=qcOutFolder;
+        mMode = mode;
+        mHostURL = hostURL;
     }
 
     /**
@@ -55,152 +46,190 @@ public class ModisLSTDownloader extends DownloaderFramework{
      * earlier than the specified start date.
      *
      * @param startDate
-     *            If non-null, specifies the inclusive lower bound for the
-     *            returned data date list; if null, data dates are not filtered
+     *            Specifies the inclusive lower bound for the returned data date
+     *            list
      * @throws IOException
+     * @throws SAXException
+     * @throws ParserConfigurationException
      */
-    public static final List<DataDate> listDates(DataDate startDate)
-            throws IOException {
-        String mode = metaData.mode;
-        Object conn =
-                ConnectionContext.getConnection(mode, "ModisLST",metaData);
-        if(mode=="HTTP")
+    protected static final List<DataDate> listDates(final DataDate startDate)
+            throws ConfigReadException, IOException, ParserConfigurationException, SAXException {
+        final List<DataDate> desiredDates = new ArrayList<DataDate>();
+        final Lock lock = new ReentrantLock();
+
+        try
         {
-            final Pattern re = Pattern.compile("(\\d{4})\\.(\\d{2})\\.(\\d{2})(/)");
-            byte[] downloadPage = null;
-            try {
-                downloadPage =
-                        DownloadUtils.downloadToByteArray((URLConnection) conn);
-            } catch (ConnectException e) {
-                throw e;
-            }
-            final HtmlDocumentBuilder builder = new HtmlDocumentBuilder();
-            Document pagedoc = null;
-            try {
-                pagedoc = builder.parse(new ByteArrayInputStream(downloadPage));
-            } catch (SAXException e) {
-                throw new IOException(
-                        "Failed to parse the ModisLST download page", e);
-            }
+            ByteArrayOutputStream folderOutStream = new ByteArrayOutputStream();
+            DownloadUtils.downloadToStream(new URL(mHostURL), folderOutStream);
 
-            final NodeList dirlist = pagedoc.getElementsByTagName("a");
+            Iterable<String> availableDates = Arrays.asList(folderOutStream.toString().split("[\\r\\n]+"));
 
-            final List<DataDate> list = new ArrayList<DataDate>();
+            Parallel.ForEach(availableDates,
+                    new Parallel.Operation<String>() {
+                @Override
+                public void perform(String param)
+                {
+                    String datePattern = "((19|20)\\d\\d).(0?[1-9]|1[012]).(0?[1-9]|[12][0-9]|3[01])/";
+                    Pattern pattern = Pattern.compile(datePattern);
+                    Matcher matcher = pattern.matcher(param);
 
-            for (int i = 0; i < dirlist.getLength(); ++i) {
-                final String dir =
-                        ((Element) dirlist.item(i)).getAttribute("href");
+                    if(matcher.find())
+                    {
+                        try
+                        {
+                            int year = Integer.parseInt(matcher.group(1));
+                            int month = Integer.parseInt(matcher.group(3));
+                            int day = Integer.parseInt(matcher.group(4));
 
-                // Match the filename against the known pattern of a MODIS NBAR
-                // date directory
-                final Matcher matcher = re.matcher(dir);
-                if (matcher.matches()) {
-                    final int year = Integer.parseInt(matcher.group(1));
-                    final int month = Integer.parseInt(matcher.group(2));
-                    final int day = Integer.parseInt(matcher.group(3));
-
-                    final DataDate dataDate = new DataDate(day, month, year);
-                    if (startDate == null || dataDate.compareTo(startDate) >= 0) {
-                        list.add(dataDate);
+                            if(year >= startDate.getYear() && month >= startDate.getMonth() && day >= startDate.getDay())
+                            {
+                                lock.lock();
+                                desiredDates.add(new DataDate(day, month, year));
+                                lock.unlock();
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                            return;
+                        }
                     }
-                }
-            }
-
-            return list;
-        }else {
-            // TODO:FTP
+                };
+            });
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
             return null;
         }
-    }
 
+        return desiredDates;
+    }
 
     @Override
     public final void download() throws IOException, ConfigReadException,
-    DownloadFailedException, SAXException, Exception {
-        String mode = metaData.mode;
-        // Build a regular expression that will match any HDF files with the specified date and tile
+    DownloadFailedException, SAXException, Exception
+    {
+        if(!(new File(mOutFolder).exists())) {
+            FileUtils.forceMkdir(new File(mOutFolder));
+        }
+        if(!(new File(mOutQCFolder).exists())) {
+            FileUtils.forceMkdir(new File(mOutQCFolder));
+        }
+        if (mMode == "HTTP")
+        {
+            try
+            {
+                String urlString = getFileURL("MOD11A2.005");
+                URL fileURL = new URL(urlString);
 
-        // (and any processing date)
-        if (mode == "HTTP"){
-            final Pattern re = Pattern.compile(String.format(
-                    "%s\\.A%04d%03d\\.h%02dv%02d\\.005\\.\\d{13}\\.hdf",
-                    getProduct(),
-                    mDate.getYear(),
-                    mDate.getDayOfYear(),
-                    mTile.getHTile(),
-                    mTile.getVTile()
-                    // mProcessed.getYear(),
-                    // mProcessed.getDayOfYear()
-                    ));
-            String url_str = getModisDateDir(getRootDir(), mDate);
-            URL url = new URL(url_str);
+                if(fileURL != null)
+                {
+                    DownloadUtils.downloadToFile(fileURL,
+                            new File(mOutFolder + "\\" + urlString.substring(urlString.lastIndexOf("MOD11A2"))));
 
-            // list the files
-            // catch and throw connectionException.
-            byte[] downloadPage = null;
-            try {
-                downloadPage = DownloadUtils.downloadToByteArray(url);
-            }catch(ConnectException e){
-                throw e;
-            }
-
-            // Parse it into a DOM tree
-            final HtmlDocumentBuilder builder = new HtmlDocumentBuilder();
-            Document pagedoc = null;
-            try {
-                pagedoc = builder.parse(new ByteArrayInputStream(downloadPage));
-            } catch (SAXException e) {
-                throw new IOException(
-                        "Failed to parse the ModisLST download page", e);
-            }
-
-            final NodeList dirlist = pagedoc.getElementsByTagName("a");
-
-            try {
-                // List files and select the best match
-                String bestMatch = null;
-                for (int i = 0; i < dirlist.getLength(); ++i) {
-                    final String dir =
-                            ((Element) dirlist.item(i)).getAttribute("href");
-
-                    if (re.matcher(dir).matches()
-                            && (bestMatch == null || dir.compareTo(bestMatch) > 0)) {
-                        // This file is either the first match or has a more
-                        // recent processing date than the current best match
-                        bestMatch = dir;
-                    }
+                    File f1=new File(mOutFolder + "\\" + urlString.substring(urlString.lastIndexOf("MOD11A2")));
+                    File f2=new File(mOutQCFolder + "\\" + urlString.substring(urlString.lastIndexOf("MOD11A2")));
+                    copyFileUsingJava7Files(f1,f2);
                 }
-
-                if (bestMatch == null) {
-                    throw new FileNotFoundException();
-                }
-
-                url_str += "/" + bestMatch;
-                // Download the archive
-                url = new URL(url_str);
-
-                DownloadUtils.downloadToFile(url, mOutFile);
-
-            } catch (IOException e) { // FIXME: ugly fix so that the system
-                // doesn't repeatedly try and fail
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-                throw e;
             }
+            catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+                return;
+            }
+            catch (IOException eIO)
+            {
+                eIO.printStackTrace();
+                return;
+            }
+        }
+        else if(mMode == "FTP")
+        {
+            // UNDO
         }
     }
 
-
-    protected String getRootDir() {
-        // TODO: handle better
-        try {
-            return Config.getInstance().getModisLstUrl();
-        } catch (ConfigReadException e) {
-            return "";
-        }
+    private static void copyFileUsingJava7Files(File source, File dest)  throws IOException {
+        Files.copy(source.toPath(), dest.toPath());
     }
 
+    public final List<String> getAllUrlsFromStartDate(DataDate startDate, final ModisTile desiredTile, final String hostURL) throws MalformedURLException, IOException
+    {
+        final ArrayList<String> fullURLs = new ArrayList<String>();
+        final Lock lock = new ReentrantLock();
+
+        try
+        {
+            List<DataDate> desiredDates = listDates(startDate);
+            if(desiredDates != null && !desiredDates.isEmpty())
+            {
+                // Parallelizing was approximately 40% faster than sequential (Getting the stream is the primary bottleneck)
+                Parallel.ForEach(desiredDates,
+                        new Parallel.Operation<DataDate>() {
+                    @Override
+                    public void perform(DataDate date)
+                    {
+                        try
+                        {
+                            ByteArrayOutputStream filesOutStream = new ByteArrayOutputStream();
+                            DownloadUtils.downloadToStream(new URL(hostURL + String.format("%04d.%02d.%02d/", date.getYear(), date.getMonth(), date.getDay())), filesOutStream);
+
+                            Iterable<String> folderContents = Arrays.asList(filesOutStream.toString().split("[//r//n]+"));
+
+                            for (String line : folderContents)
+                            {
+                                //Shorten the list as much as possible, restricting to only files that have ".hdf" extension
+                                if(line.contains(".hdf") && !line.contains(".xml") &&
+                                        line.contains(String.format("h%02dv%02d", desiredTile.getHTile(), desiredTile.getVTile())))
+                                {
+                                    lock.lock();
+                                    fullURLs.add(hostURL + String.format("%04d.%02d.%.02d/", date.getYear(), date.getMonth(), date.getDay()) + line.substring(line.indexOf("MOD11A2"), line.indexOf(".hdf") + 4));
+                                    lock.unlock();
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    };
+                });
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+        return fullURLs;
+    }
+
+    private String getFileURL(String parentFolder) throws MalformedURLException, IOException
+    {
+        String fileURL = null;
+
+        String containingURL = mHostURL + String.format("%s/%04d.%02d.%02d/", parentFolder, mDate.getYear(), mDate.getMonth(), mDate.getDay());
+        ByteArrayOutputStream directoryOutstream = new ByteArrayOutputStream();
+        DownloadUtils.downloadToStream(new URL(containingURL), directoryOutstream);
+
+        Iterable<String> folderContents = Arrays.asList(directoryOutstream.toString().split("[//r//n]+"));
+        for (String line : folderContents)
+        {
+            if(line.contains(".hdf") && !line.contains(".xml") &&
+                    line.contains(String.format("h%02dv%02d", mTile.getHTile(), mTile.getVTile())))
+            {
+                fileURL = (containingURL + line.substring(line.indexOf(parentFolder.substring(0, parentFolder.indexOf('.'))), line.indexOf(".hdf") + 4));
+                break;
+            }
+        }
+        return fileURL;
+    }
 }
+
+
