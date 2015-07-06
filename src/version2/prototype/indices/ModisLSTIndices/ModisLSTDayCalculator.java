47b02766dfafd08f5d9166ce54c31541b760a8cb
fangyu <bravefishing@gmail.com>
2015-07-06 09:10:24 -0500

    change some syntax in modisLST indices

---
+++ src/version2/prototype/indices/ModisLSTIndices/ModisLSTDayCalculator.java
@@ -0,0 +1,29 @@
+package version2.prototype.indices.ModisLSTIndices;
+
+import version2.prototype.indices.IndicesFramework;
+import version2.prototype.util.GeneralListener;
+
+public class ModisLSTDayCalculator extends IndicesFramework{
+
+    final static int INPUT = 0;
+    double mMin;
+    double mMax;
+
+    public ModisLSTDayCalculator(GeneralListener l){
+        super(l);
+    }
+
+    @Override
+    protected double calculatePixelValue(double[] values) {
+        if (values[INPUT] < mMin || values[INPUT] > mMax) {
+            return -3.4028234663852886E38;
+        } else {
+            return values[INPUT];
+        }
+    }
+
+    @Override
+    protected String className() {
+        return getClass().getName();
+    }
+}
