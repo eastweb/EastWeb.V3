518bab58ecd971458525c8abe789badf0d4e2be9
liuyi-j <liuyi_j@yahoo.com>
2015-07-14 01:08:09 -0500

    Test TRMM3B42RTListDatesFiles

--- src/test/download/TRMM3B42RT/TRMM3B42RTListDatesFilesTest.java
+++ src/test/download/TRMM3B42RT/TRMM3B42RTListDatesFilesTest.java
@@ -26,24 +26,55 @@ import version2.prototype.download.TRMM3B42RT.TRMM3B42RTListDatesFiles;
  *
  */
 public class TRMM3B42RTListDatesFilesTest {
+
     private static DataDate date;
     private static DownloadMetaData data;
+
+    public static void main(String [] args) throws Exception
+    {
+        setUpBeforeClass();
+
+        try {
+            //testListDatesFilesFTP();
+
+            TRMM3B42RTListDatesFiles ldates = new TRMM3B42RTListDatesFiles(date, data);
+
+            Map<DataDate, ArrayList<String>> datesFiles = ldates.getListDatesFiles();
+
+            System.out.println(datesFiles.size());
+
+            for (Map.Entry<DataDate, ArrayList<String>> entry : datesFiles.entrySet())
+            {
+                System.out.println(entry.getKey() + " : /" + entry.getValue().get(0));
+            }
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        tearDownAfterClass();
+
+    }
 
     /**
      * @throws java.lang.Exception
      */
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        date = new DataDate(LocalDate.now().minusDays(1));
+        // date = new DataDate(LocalDate.now().minusDays(1));
+        date = new DataDate(22, 3, 2015);
 
-        String mode = null;// the protocol type: ftp or http
-        FTP myFtp = null;
+        String mode = "FTP";// the protocol type: ftp or http
+        FTP myFtp = PluginMetaDataCollection.CreateFTP("disc2.nascom.nasa.gov",
+                "/data/TRMM/Gridded/Derived_Products/3B42RT/Daily/", "anonymous", "anonymous");
         HTTP myHttp = null;
         String className = null;
         String timeZone = null;
         int filesPerDay = -1;
-        String datePatternStr = null;
-        String fileNamePatternStr = null;
+        String datePatternStr = "\\d{4}";
+        String fileNamePatternStr = "3B42RT_daily\\.(\\d{4})\\.(\\d{2})\\.(\\d{2})\\.bin";
         data = PluginMetaDataCollection.CreateDownloadMetaData(mode, myFtp, myHttp, className, timeZone, filesPerDay, datePatternStr, fileNamePatternStr);
     }
 
@@ -61,7 +92,7 @@ public class TRMM3B42RTListDatesFilesTest {
      * @throws IOException
      */
     @Test
-    public final void testListDatesFilesFTP() throws IOException {
+    public final static void testListDatesFilesFTP() throws IOException {
         TRMM3B42RTListDatesFiles testy = new TRMM3B42RTListDatesFiles(date, data);
 
         Map<DataDate, ArrayList<String>> files = testy.getListDatesFiles();
