7cb2a38db75a572ab94722a1220fd147023f4b78
eastweb <eastweb.epidemia.data@gmail.com>
2015-07-17 16:49:55 -0500

    TestUnit - NldasNOAH

---
+++ src/test/download/NldasNOAH/TestNldasNOAHListDatesFiles.java
@@ -0,0 +1,32 @@
+package test.download.NldasNOAH;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestNldasNOAHListDatesFiles {
+
+    public TestNldasNOAHListDatesFiles() {
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Test
+    public void testListDatesFilesFTP() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testNldasNOAHListDatesFiles() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
