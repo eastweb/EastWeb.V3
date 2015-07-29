37be253e52d13cb7626be3dc811511b9f668f4be
fangyu <bravefishing@gmail.com>
2015-07-29 09:09:21 -0500

    remove mMin and mMax and modify the bad pixel value as GdalUtils.NoValue not 32767

--- src/version2/prototype/indices/ModisLSTIndices/ModisLSTNightCalculator.java
+++ src/version2/prototype/indices/ModisLSTIndices/ModisLSTNightCalculator.java
@@ -1,17 +1,17 @@
 package version2.prototype.indices.ModisLSTIndices;
 
 import version2.prototype.indices.IndicesFramework;
+import version2.prototype.util.GdalUtils;
 
 public class ModisLSTNightCalculator extends IndicesFramework {
     final static int INPUT = 0;
-    double mMin;
-    double mMax;
 
+    // valid range for data value are from 7500 to 65535
     public ModisLSTNightCalculator(){}
 
     @Override
     protected double calculatePixelValue(double[] values) {
-        if (values[INPUT] < mMin || values[INPUT] > mMax) {
+        if (values[INPUT] < 7500 || values[INPUT] > 65535||values[INPUT] == GdalUtils.NoValue) {
             return -3.4028234663852886E38;
         } else {
             return values[INPUT];
