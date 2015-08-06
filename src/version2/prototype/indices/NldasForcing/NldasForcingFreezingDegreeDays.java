1c153b7db0de36c96e2e3dff3e6bb0559a28240c
chrisplucker <christopher.plucker18749@jacks.sdstate.edu>
2015-08-06 09:53:20 -0500

    Update NldasForcingFreezingDegreeDays.java

--- src/version2/prototype/indices/NldasForcing/NldasForcingFreezingDegreeDays.java
+++ src/version2/prototype/indices/NldasForcing/NldasForcingFreezingDegreeDays.java
@@ -22,7 +22,7 @@ public class NldasForcingFreezingDegreeDays extends IndicesFramework {
     {
         for(File input : mInputFiles)
         {
-            if(input.getName().contains("FreezingDegreeDays_"))
+            if(input.getName().equals("FreezingDegreeDays"))
             {
                 FileUtils.forceMkdir(mOutputFile.getParentFile());
                 if(!input.renameTo(mOutputFile)) {
