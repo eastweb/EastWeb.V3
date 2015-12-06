0355b7eb639da47fc07b657456a06f8c73112284
sufiabdul <muhammad.abdulrahim@jacks.sdstate.edu>
2015-12-06 00:24:00 -0600

    update setting UI

--- src/version2/prototype/EastWebUI_V2/SettingsUI_v2/SettingsUI.java
+++ src/version2/prototype/EastWebUI_V2/SettingsUI_v2/SettingsUI.java
@@ -9,6 +9,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
+import javax.swing.JTree;
 import javax.swing.UIManager;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -31,6 +32,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import javax.swing.JTextArea;
+import javax.swing.JTabbedPane;
 
 public class SettingsUI {
     private JFrame frame;
@@ -44,6 +46,9 @@ public class SettingsUI {
     private JTextField textFieldPassword;
     private JTextField textFieldMaxNumOfConnectionsPerInstance;
     private JTextArea textArea;
+    private JTree tree;
+    private JScrollPane qPane;
+    private JTabbedPane tabbedPane;
 
     private boolean isAdvance = false;
 
@@ -72,26 +77,16 @@ public class SettingsUI {
         initializeUIValue();
     }
 
-    private void initializeUIValue() {
-        errorLogDirTextField.setText(Config.getInstance().getErrorLogDir());
-        downloadDirtextField.setText(Config.getInstance().getDownloadDir());
-        GlobalSchematextField.setText(Config.getInstance().getGlobalSchema());
-        textFieldHostName.setText(Config.getInstance().getDatabaseHost());
-        textFieldPort.setText(Config.getInstance().getPort().toString());
-        textFieldDatabaseName.setText(Config.getInstance().getDatabaseName());
-        textFieldUserName.setText(Config.getInstance().getDatabaseUsername());
-        textFieldPassword.setText(Config.getInstance().getDatabasePassword());
-        textFieldMaxNumOfConnectionsPerInstance.setText(Config.getInstance().getMaxNumOfConnectionsPerInstance().toString());
-    }
-
     /**
      * Initialize the contents of the frame.
+     * @throws IOException
      */
-    private void initialize() {
+    private void initialize() throws IOException {
         frame = new JFrame();
         frame.setVisible(true);
-        frame.setBounds(100, 100, 545, 395);
+        frame.setBounds(100, 100, 545, 750);
         frame.getContentPane().setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel lblProjectInformation = new JLabel("System Settings");
         lblProjectInformation.setFont(new Font("Courier", Font.BOLD,25));
@@ -106,24 +101,10 @@ public class SettingsUI {
         saveButton.setIcon(new ImageIcon(ProjectInformationPage.class.getResource("/version2/prototype/Images/save_32.png")));
         saveButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent arg0) {
-                try {
-                    if(Config.getInstance().WriteConfigFile(GetXMLObject()) && Config.getInstance().Writec3p0File(textArea.getText())){
-                        JOptionPane.showMessageDialog(frame, "Settings was saved");
-                        System.out.println("File saved!");
-                    }else{
-                        System.out.println("Erorr in saving");
-                    }
-                } catch (HeadlessException | ParserConfigurationException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                frame.dispose();
-            }
+            public void actionPerformed(ActionEvent arg0) {saveConfig();}
         });
         saveButton.setToolTipText("Save Settings");
         frame.getContentPane().add(saveButton);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel lblErrorLogDir = new JLabel("Error Log Directory:");
         lblErrorLogDir.setBounds(10, 60, 143, 14);
@@ -131,8 +112,8 @@ public class SettingsUI {
 
         errorLogDirTextField = new JTextField();
         errorLogDirTextField.setBounds(163, 57, 257, 25);
-        frame.getContentPane().add(errorLogDirTextField);
         errorLogDirTextField.setColumns(10);
+        frame.getContentPane().add(errorLogDirTextField);
 
         JButton browseErrorLogDirButton = new JButton("");
         browseErrorLogDirButton.setBounds(430, 54, 27, 23);
@@ -171,80 +152,90 @@ public class SettingsUI {
         advanceUI();
     }
 
+    private void initializeUIValue() {
+        errorLogDirTextField.setText(Config.getInstance().getErrorLogDir());
+        downloadDirtextField.setText(Config.getInstance().getDownloadDir());
+        GlobalSchematextField.setText(Config.getInstance().getGlobalSchema());
+        textFieldHostName.setText(Config.getInstance().getDatabaseHost());
+        textFieldPort.setText(Config.getInstance().getPort().toString());
+        textFieldDatabaseName.setText(Config.getInstance().getDatabaseName());
+        textFieldUserName.setText(Config.getInstance().getDatabaseUsername());
+        textFieldPassword.setText(Config.getInstance().getDatabasePassword());
+        textFieldMaxNumOfConnectionsPerInstance.setText(Config.getInstance().getMaxNumOfConnectionsPerInstance().toString());
+    }
+
     private void databaseUI() {
         JPanel databasePanel = new JPanel();
         databasePanel.setBorder(new TitledBorder(null, "Database Information", TitledBorder.LEADING, TitledBorder.TOP, null, null));
         databasePanel.setBounds(10, 110, 509, 207);
-        frame.getContentPane().add(databasePanel);
         databasePanel.setLayout(null);
+        frame.getContentPane().add(databasePanel);
 
         JLabel labelGlobalSchema = new JLabel("Global Schema");
         labelGlobalSchema.setBounds(10, 24, 153, 14);
         databasePanel.add(labelGlobalSchema);
 
+        GlobalSchematextField = new JTextField();
+        GlobalSchematextField.setBounds(173, 18, 238, 27);
+        GlobalSchematextField.setColumns(10);
+        databasePanel.add(GlobalSchematextField);
+
         JLabel labelHostName = new JLabel("Host Name");
         labelHostName.setBounds(10, 49, 153, 14);
         databasePanel.add(labelHostName);
+
+        textFieldHostName = new JTextField();
+        textFieldHostName.setBounds(173, 43, 238, 27);
+        textFieldHostName.setColumns(10);
+        databasePanel.add(textFieldHostName);
 
         JLabel labelPort = new JLabel("Port");
         labelPort.setBounds(10, 74, 153, 14);
         databasePanel.add(labelPort);
 
+        textFieldPort = new JTextField();
+        textFieldPort.setBounds(173, 68, 238, 27);
+        textFieldPort.setColumns(10);
+        databasePanel.add(textFieldPort);
+
         JLabel labelDatabaseName = new JLabel("Database Name");
         labelDatabaseName.setBounds(10, 99, 153, 14);
         databasePanel.add(labelDatabaseName);
+
+        textFieldDatabaseName = new JTextField();
+        textFieldDatabaseName.setBounds(173, 93, 238, 27);
+        textFieldDatabaseName.setColumns(10);
+        databasePanel.add(textFieldDatabaseName);
 
         JLabel labelUserName = new JLabel("User Name");
         labelUserName.setBounds(10, 124, 153, 14);
         databasePanel.add(labelUserName);
 
+        textFieldUserName = new JTextField();
+        textFieldUserName.setBounds(173, 118, 238, 27);
+        textFieldUserName.setColumns(10);
+        databasePanel.add(textFieldUserName);
+
         JLabel labelPassword = new JLabel("Password");
         labelPassword.setBounds(10, 149, 153, 14);
         databasePanel.add(labelPassword);
+
+        textFieldPassword = new JTextField();
+        textFieldPassword.setBounds(173, 143, 238, 27);
+        textFieldPassword.setColumns(10);
+        databasePanel.add(textFieldPassword);
 
         JLabel labelMaxNumOfConnectionsPerInstance = new JLabel("Connections Per Instance");
         labelMaxNumOfConnectionsPerInstance.setBounds(10, 174, 153, 14);
         databasePanel.add(labelMaxNumOfConnectionsPerInstance);
 
-        GlobalSchematextField = new JTextField();
-        GlobalSchematextField.setBounds(173, 18, 238, 27);
-        databasePanel.add(GlobalSchematextField);
-        GlobalSchematextField.setColumns(10);
-
-        textFieldHostName = new JTextField();
-        textFieldHostName.setBounds(173, 43, 238, 27);
-        databasePanel.add(textFieldHostName);
-        textFieldHostName.setColumns(10);
-
-        textFieldPort = new JTextField();
-        textFieldPort.setBounds(173, 68, 238, 27);
-        databasePanel.add(textFieldPort);
-        textFieldPort.setColumns(10);
-
-        textFieldDatabaseName = new JTextField();
-        textFieldDatabaseName.setBounds(173, 93, 238, 27);
-        databasePanel.add(textFieldDatabaseName);
-        textFieldDatabaseName.setColumns(10);
-
-        textFieldUserName = new JTextField();
-        textFieldUserName.setBounds(173, 118, 238, 27);
-        databasePanel.add(textFieldUserName);
-        textFieldUserName.setColumns(10);
-
-        textFieldPassword = new JTextField();
-        textFieldPassword.setBounds(173, 143, 238, 27);
-        databasePanel.add(textFieldPassword);
-        textFieldPassword.setColumns(10);
-
         textFieldMaxNumOfConnectionsPerInstance = new JTextField();
         textFieldMaxNumOfConnectionsPerInstance.setColumns(10);
         textFieldMaxNumOfConnectionsPerInstance.setBounds(173, 168, 238, 27);
         databasePanel.add(textFieldMaxNumOfConnectionsPerInstance);
-
-
     }
 
-    private void advanceUI() {
+    private void advanceUI() throws IOException {
         JButton btnAdvanceSettings = new JButton("Advance Settings");
         btnAdvanceSettings.addActionListener(new ActionListener() {
             @Override
@@ -258,9 +249,12 @@ public class SettingsUI {
         lblAdvanceSystemSettings.setBounds(10, 362, 389, 32);
         frame.getContentPane().add(lblAdvanceSystemSettings);
 
+        tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        tabbedPane.setBounds(10, 422, 509, 278);
+        frame.getContentPane().add(tabbedPane);
+
         JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setBounds(10, 405, 509, 295);
-        frame.getContentPane().add(scrollPane);
+        tabbedPane.addTab("Raw Xml", null, scrollPane, null);
 
         textArea = new JTextArea();
         scrollPane.setViewportView(textArea);
@@ -272,9 +266,20 @@ public class SettingsUI {
             br.close();
 
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+        tree = new XMLTree(textArea.getText());
+        qPane = new JScrollPane(tree, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        tabbedPane.addTab("Xml hierarchy", null, qPane, null);
+
+        JButton btnUpdateModel = new JButton("Update Model");
+        btnUpdateModel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) { updateModel();}
+        });
+        btnUpdateModel.setBounds(395, 388, 124, 23);
+        frame.getContentPane().add(btnUpdateModel);
     }
 
     private void browseFolderDir(JTextField dirTextField) {
@@ -286,7 +291,6 @@ public class SettingsUI {
 
         if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
             System.out.println("getCurrentDirectory(): "+ chooser.getCurrentDirectory());
-            System.out.println("getSelectedFile() : "+ chooser.getSelectedFile());
             dirTextField.setText(chooser.getSelectedFile().toString());
         } else {
             System.out.println("No Selection ");
@@ -363,5 +367,28 @@ public class SettingsUI {
         }
 
         return doc;
+    }
+
+    private void saveConfig() {
+        try {
+            if(Config.getInstance().WriteConfigFile(GetXMLObject()) && Config.getInstance().Writec3p0File(textArea.getText())){
+                JOptionPane.showMessageDialog(frame, "Settings was saved");
+                System.out.println("File saved!");
+            }else{
+                System.out.println("Erorr in saving");
+            }
+        } catch (HeadlessException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        frame.dispose();
+    }
+
+    private void updateModel() {
+        qPane.remove(tree);
+        tabbedPane.remove(qPane);
+
+        tree = new XMLTree(textArea.getText());
+        qPane = new JScrollPane(tree, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        tabbedPane.addTab("Xml hierarchy", null, qPane, null);
     }
 }
---
+++ src/version2/prototype/EastWebUI_V2/SettingsUI_v2/XMLTree.java
@@ -0,0 +1,123 @@
+package version2.prototype.EastWebUI_V2.SettingsUI_v2;
+
+
+import javax.swing.*;
+import javax.swing.tree.*;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+import org.w3c.dom.*;
+
+import version2.prototype.EastWebUI_V2.DocumentBuilderInstance;
+
+/** Given a filename or a name and an input stream,
+ *  this class generates a JTree representing the
+ *  XML structure contained in the file or stream.
+ *  Parses with DOM then copies the tree structure
+ *  (minus text and comment nodes).
+ */
+
+@SuppressWarnings("serial")
+public class XMLTree extends JTree {
+    public XMLTree(File file) throws IOException {
+        this(new FileInputStream(file));
+    }
+
+    public XMLTree(InputStream in) {
+        super(makeRootNode(in));
+    }
+
+    public XMLTree(String text){
+        super(makeRootNode(new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8))));
+    }
+
+    // This method needs to be static so that it can be called
+    // from the call to the parent constructor (super), which
+    // occurs before the object is really built.
+    private static DefaultMutableTreeNode makeRootNode(InputStream in) {
+        try {
+            // Standard DOM code from hereon. The "parse"
+            // method invokes the parser and returns a fully parsed
+            // Document object. We'll then recursively descend the
+            // tree and copy non-text nodes into JTree nodes.
+            Document document = DocumentBuilderInstance.Instance().GetDocumentBuilder().parse(in);
+            document.getDocumentElement().normalize();
+            Element rootElement = document.getDocumentElement();
+            DefaultMutableTreeNode rootTreeNode = buildTree(rootElement);
+
+            return(rootTreeNode);
+        } catch(Exception e) {
+            String errorMessage = "Error making root node: " + e;
+            System.err.println(errorMessage);
+            e.printStackTrace();
+
+            return(new DefaultMutableTreeNode(errorMessage));
+        }
+    }
+
+    private static DefaultMutableTreeNode buildTree(Element rootElement) {
+        // Make a JTree node for the root, then make JTree
+        // nodes for each child and add them to the root node.
+        // The addChildren method is recursive.
+        DefaultMutableTreeNode rootTreeNode = new DefaultMutableTreeNode(treeNodeLabel(rootElement));
+        addChildren(rootTreeNode, rootElement);
+
+        return(rootTreeNode);
+    }
+    private static void addChildren (DefaultMutableTreeNode parentTreeNode, Node parentXMLElement) {
+        // Recursive method that finds all the child elements
+        // and adds them to the parent node. We have two types
+        // of nodes here: the ones corresponding to the actual
+        // XML structure and the entries of the graphical JTree.
+        // The convention is that nodes corresponding to the
+        // graphical JTree will have the word "tree" in the
+        // variable name. Thus, "childElement" is the child XML
+        // element whereas "childTreeNode" is the JTree element.
+        // This method just copies the non-text and non-comment
+        // nodes from the XML structure to the JTree structure.
+
+        NodeList childElements = parentXMLElement.getChildNodes();
+
+        for(int i=0; i<childElements.getLength(); i++) {
+            Node childElement = childElements.item(i);
+
+            if (!(childElement instanceof Text || childElement instanceof Comment)) {
+                DefaultMutableTreeNode childTreeNode = new DefaultMutableTreeNode(treeNodeLabel(childElement));
+                parentTreeNode.add(childTreeNode);
+                addChildren(childTreeNode, childElement);
+            }
+        }
+    }
+
+    // If the XML element has no attributes, the JTree node
+    // will just have the name of the XML element. If the
+    // XML element has attributes, the names and values of the
+    // attributes will be listed in parens after the XML
+    // element name. For example:
+    // XML Element: <blah>
+    // JTree Node:  blah
+    // XML Element: <blah foo="bar" baz="quux">
+    // JTree Node:  blah (foo=bar, baz=quux)
+
+    private static String treeNodeLabel(Node childElement) {
+        NamedNodeMap elementAttributes = childElement.getAttributes();
+        String treeNodeLabel = childElement.getNodeName();
+
+        if (elementAttributes != null && elementAttributes.getLength() > 0) {
+            treeNodeLabel = treeNodeLabel + " (";
+            int numAttributes = elementAttributes.getLength();
+
+            for(int i=0; i<numAttributes; i++) {
+                Node attribute = elementAttributes.item(i);
+
+                if (i > 0) {
+                    treeNodeLabel = treeNodeLabel + ", ";
+                }
+                treeNodeLabel = treeNodeLabel + attribute.getNodeName() + "=" + attribute.getNodeValue();
+            }
+            treeNodeLabel = treeNodeLabel + ")" + String.format("; value = %s", childElement.getTextContent());
+        }
+        return(treeNodeLabel);
+    }
+}
