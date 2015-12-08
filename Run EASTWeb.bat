048f81bdeca6c9a662ba0a43364b7978f3b058e0
michael.devos <michael.devos.jacks@gmail.com>
2015-12-08 15:11:59 -0600

    Simplified batch script runner

--- Run EASTWeb.bat
+++ Run EASTWeb.bat
@@ -1,6 +1,2 @@
 @ECHO off
-<<<<<<< Upstream, based on origin/master
-start /B "%~d0\Program Files (x86)\Java\jre1.8.0_51\bin\javaw" "EASTWeb_V2.jar"
-=======
 start /B "javaw" "EASTWeb_V2.jar"
->>>>>>> 22c682c Bug fixes, updated TaskState handling, added completed temp file cleanup
