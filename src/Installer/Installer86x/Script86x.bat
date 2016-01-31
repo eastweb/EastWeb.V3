ea74b5323784c5c74ff509ac4e821388beb90539
sufiabdul <muhammad.abdulrahim@jacks.sdstate.edu>
2016-01-30 23:51:59 -0600

    update installer

---
+++ src/Installer/Installer86x/Script86x.bat
@@ -0,0 +1,21 @@
+@echo off
+
+rem delete old output and project
+@RD /S /Q "%~dp0..\Installer64x\Output"
+@RD /S /Q "%~dp0..\Installer86x\Output"
+pause
+
+rem copy project to desktop
+robocopy "%~dp0..\..\..\..\EastWeb.V2" "%temp%\EastWeb.V2" /s /xf *.iss *exe *bat
+pause
+
+rem copy project from desktop to installer folder (this prevents infinite loop)
+rem then remove project form desktop
+robocopy "%temp%\EastWeb.V2" "%~dp0..\EastWeb.V2" /mir
+@RD /S /Q "%temp%\EastWeb.V2"
+pause
+
+rem compile installer 
+"%~dp0..\InstallerCompiler\IScc.exe" %~dp0EastWeb86x.iss
+@RD /S /Q "%~dp0..\EastWeb.V2"
+pause
