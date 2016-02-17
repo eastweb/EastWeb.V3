29b861f94cf1665fbffbaa408f7c16516c98b494
michael.devos <michael.devos.jacks@gmail.com>
2016-02-17 09:37:00 -0600

    Updated batch files and modified ErrorLog

--- Run EASTWeb.bat
+++ Run EASTWeb.bat
@@ -1,2 +1,4 @@
 @ECHO off
-start /B "javaw" "EASTWeb_V2.jar"
+:: start /B "C:\Program Files (x86)\Java\jre1.8.0_31\bin\javaw -jar" EASTWeb_V2.jar
+ECHO Starting EASTWeb...
+"C:\Program Files (x86)\Java\jre1.8.0_66\bin\javaw" -jar EASTWeb_V2.jar
--- Run TestDatabaseConnection.bat
+++
@@ -1,3 +0,0 @@
-@ECHO off
-java -jar .\TestDatabaseConnection.jar .\config\config.xml
-PAUSE
